7ed5ecc500c35fd46799bb230e6f93a7e90a0dfc
Tim Lanzi <trlanzi@gmail.com>
2021-03-28 16:09:39 -0400

    Fix display bug with privacy policy link

--- web/src/components/Layout/PrivacyLink.tsx
+++ web/src/components/Layout/PrivacyLink.tsx
@@ -1,9 +1,19 @@
 import { Flex, Box, Text } from "@chakra-ui/react"
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const PrivacyLink = () => {
+const PrivacyLink: React.FC = () => {
+  const router = useRouter();
   return (
-    <Flex pos="absolute" bottom="0" right="0">
+    <Flex
+    pos={{
+      base: router.pathname === "/"
+        ? "fixed"
+        : "relative",
+      "2xl": "fixed",
+    }}
+    bottom="0"
+    left="0">
       <Box p="5">
         <Link href="privacy">
           <Text as="span" _hover={{ color: "blue.500", cursor: "pointer", textDecoration: "underline"  }}>
